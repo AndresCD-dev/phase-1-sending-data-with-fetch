4c39a4a928ad411cc10f83ddd79ed1be6b1d4844
andres <andres.gordillo@mail.citytech.cuny.edu>
2021-07-31 19:37:46 -0400

    commit tree 8f15140fb3be03a7035353534ec3d8219d11a48a

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            appDom(data)
+
+
+
+        })
+        .catch(err => appError(err))
+}
+function appDom(object) {
+    const body = document.querySelector("body")
+    body.append(object.id);
+}
+function appError(error){
+    const body = document.querySelector("body")
+    body.append(error)
+}
